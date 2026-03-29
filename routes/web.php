1fd32b8bb406420a2e3227631ffefa4af8773ba7
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2026-03-29 16:20:59 +0700

    feat: add routes for transactions and categories

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,36 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TransactionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Categories
+    Route::resource('categories', CategoryController::class);
+    // Products
+    Route::resource('products', ProductController::class);
+    // Kasir
+    Route::get('/kasir', [TransactionController::class, 'index'])->name('kasir.index');
+    // Transaksi
+    Route::post('/kasir/checkout', [TransactionController::class, 'store'])->name('kasir.checkout');
+    // Struk
+    Route::get('/transactions/{id}', [TransactionController::class, 'show'])->name('transactions.show');
+    
+});
+
+
+require __DIR__.'/auth.php';
